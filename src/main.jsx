f280a73b13f9a495e9ce9846b774e7a756abb8f8
ejc55 <cabada.eric7@gmail.com>
2025-10-02 21:08:51 -0700

    fix: switch to HashRouter for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,65 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// import App from "./App.jsx";
+// import Search from "./pages/Search.jsx";
+// import Home from "./pages/Home.jsx";
+// import BeginnerQuiz from "./pages/BeginnerQuiz.jsx";
+// import AdvancedQuiz from "./pages/AdvancedQuiz.jsx";
+// import Resources from "./pages/Resources.jsx"; // Import the new page
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     children: [
+//       {
+//         index: true,
+//         element: <Home />,
+//       },
+//       {
+//         path: "search",
+//         element: <Search />,
+//       },
+//       {
+//         path: "quiz/beginner",
+//         element: <BeginnerQuiz />,
+//       },
+//       {
+//         path: "quiz/advanced",
+//         element: <AdvancedQuiz />,
+//       },
+//       {
+//         path: "resources", // Add the new route
+//         element: <Resources />,
+//       },
+//     ],
+//   },
+// ]);
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <RouterProvider router={router} />
+//   </React.StrictMode>,
+// );
+
+//TEMP FOR GH PAGES:
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// 1. Change the import from createBrowserRouter to createHashRouter
+import { createHashRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App.jsx";
 import Search from "./pages/Search.jsx";
 import Home from "./pages/Home.jsx";
 import BeginnerQuiz from "./pages/BeginnerQuiz.jsx";
 import AdvancedQuiz from "./pages/AdvancedQuiz.jsx";
-import Resources from "./pages/Resources.jsx"; // Import the new page
+import Resources from "./pages/Resources.jsx";
 
-const router = createBrowserRouter([
+// 2. Change this function call
+const router = createHashRouter([
   {
     path: "/",
     element: <App />,
@@ -31,7 +81,7 @@ const router = createBrowserRouter([
         element: <AdvancedQuiz />,
       },
       {
-        path: "resources", // Add the new route
+        path: "resources",
         element: <Resources />,
       },
     ],
